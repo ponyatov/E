2ea157bca60934e3960d27e37f651c017922ddee
Dmitry Ponyatov <dponyatov@gmail.com>
2015-06-18 14:20:06 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- 2del/File.h
+++
@@ -1,9 +0,0 @@
-// File
-
-#ifndef _H_FILE
-#define _H_FILE
-
-class File {
-};
-
-#endif // _H_FILE
---
+++ File.h
@@ -0,0 +1,9 @@
+#ifndef _E_FILE
+#define _E_FILE
+
+class EFile {
+	int Handle;
+	char *Name;
+};
+
+#endif // _E_FILE
