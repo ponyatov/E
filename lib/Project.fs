8c922810a9d920300c06ece6a2560fa2c7f7abde
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-29 11:52:30 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Project.fs
+++ lib/Project.fs
@@ -5,10 +5,34 @@ let AUTHOR = "Dmitry Ponyatov"
 let EMAIL = "dponyatov@gmail.com"
 let YEAR = 2025
 let LICENSE = "MIT"
+let GITHUB = "https://github.com/ponyatov"
 
-let ABOUT = "
+let ABOUT =
+    "
 - smart vehicles, industrial automation & IIoT
 - targets microcontrollers & embedded Linux
 - heterogenous distributed systems
 - wireless sensor networks
 "
+
+open System.IO
+
+let README () =
+    File.WriteAllText(
+        "README.md",
+        $"\
+# ![logo](vscode/logo.png) `{APP}` {VERSION}
+## {TITLE}
+
+(c) {AUTHOR} <{EMAIL}> {YEAR} {LICENSE}
+
+github: {GITHUB}/{APP}
+{ABOUT}"
+    )
+
+let tmp () =
+    for d in [ "bin"; "tmp"; "ref" ] do
+        Directory.CreateDirectory(d)
+        File.WriteAllText($"{d}/.gitignore", "*\n!.gitignore\n")
+
+let dirs () = vscode doc lib inc src
