f712f63801f5d1c834e136deb084f12994426102
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-29 13:19:06 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Project.fs
+++ lib/Project.fs
@@ -513,7 +513,20 @@ include_directories(${INC})
 
 let cross () =
     File.WriteAllText($"cmake/cross.cmake",
-"""""")
+"""# cross compiler binaries: elf/dfu
+
+set(ELF ${CMAKE_INSTALL_PREFIX}/${BIN_OUTPUT_NAME}.elf)
+set(DFU ${CMAKE_INSTALL_PREFIX}/${BIN_OUTPUT_NAME}.dfu)
+
+add_custom_command(
+    OUTPUT  ${DFU}
+    DEPENDS ${ELF}
+    COMMAND ../elf2dfuse/bin/elf2dfuse ${ELF} ${DFU}
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+)
+add_custom_target(dfu DEPENDS ${DFU})
+""")
+
 let syntax () =
     File.WriteAllText($"cmake/syntax.cmake",
 """""")
