0ce4118b61f9fcad753f54fec7af224a8112f74c
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-29 13:09:02 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Project.fs
+++ lib/Project.fs
@@ -348,4 +348,83 @@ let rust () =
     File.WriteAllText($"src/main.rs", "fn main() {}\n")
     File.WriteAllText($"src/lib.rs", "")
 
-let dirs () = vscode doc src tmp giti rust
+let cpp () =
+    File.WriteAllText($"inc/hpp.hpp", "#pragma once\n")
+    File.WriteAllText($"src/cpp.cpp", "#include \"hpp.hpp\"\nint main() {}\n")
+
+let CMakeLists () =
+    File.WriteAllText("CMakeLists.txt","""cmake_minimum_required(VERSION 3.22)
+get_filename_component(CMAKE_PROJECT_NAME ${CMAKE_SOURCE_DIR} NAME_WE)
+project(${CMAKE_PROJECT_NAME} LANGUAGES C CXX ASM)
+
+include(version)        # binary files naming by version & git branch/hash
+include(src)            # scan project for source code files
+include(syntax)         # parser generators (flex,yacc/bison,ragel,lemon,..)
+
+message("-- |")
+message("-- | toolchain: " ${CMAKE_CXX_COMPILER} " @ " ${CMAKE_TOOLCHAIN_FILE})
+message("-- |      host: " ${CMAKE_HOST_SYSTEM_NAME}-${CMAKE_HOST_SYSTEM_VERSION})
+message("-- |    target: " "hw:" ${HW} " cpu:" ${CPU} " arch:" ${ARCH} " os:" ${OS})
+message("-- |   startup: " "${S}")
+message("-- |    linker: " "${LD}")
+message("-- |    binary: " ${CMAKE_INSTALL_PREFIX}/${BIN_OUTPUT_NAME}${CMAKE_EXECUTABLE_SUFFIX})
+message("-- |")
+
+add_executable(${CMAKE_PROJECT_NAME}
+    ${C}  ${H}          # C/C++ source
+    ${S}  ${LD}         # embedded/lowlevel
+    ${CP} ${HP} ${OP}   # parsers
+)
+
+# target_link_libraries(${CMAKE_PROJECT_NAME} -static)
+
+include(install)        # target install
+include(cross)          # cross compiler binaries: elf/dfu
+include(clean)          # project clean-up (remove generated & temp files)
+""")
+
+let CMakePresets () =
+    File.WriteAllText("CMakePresets.json","""{
+    "version": 6,
+    "buildPresets": [
+        {
+            "name"            :  "linux",
+            "configurePreset" :  "linux",
+            "targets"         : ["all","install"]
+        }
+    ],
+    "configurePresets": [
+        {
+            "name"            : "common",
+            "hidden"          :  true,
+            "binaryDir"       : "${sourceDir}/tmp/${presetName}",
+            "generator"       : "Unix Makefiles",
+            "cacheVariables"  : {
+                "CMAKE_INSTALL_PREFIX"    : "${sourceDir}/bin",
+                "CMAKE_MODULE_PATH"       : "${sourceDir}/cmake",
+                "CMAKE_COLOR_DIAGNOSTICS" :  false,
+                "CMAKE_BUILD_TYPE"        : "Debug",
+                "CMAKE_VERBOSE_MAKEFILE"  :  false
+            }
+        },
+        {
+            "name"            : "pc",
+            "inherits"        : "common",
+            "hidden"          :  true,
+            "cacheVariables"  : {"HW":"pc", "CPU":"i5", "ARCH":"x86_64"}
+        },
+        {
+            "name"            : "linux",
+            "inherits"        : "pc",
+            "toolchainFile"   : "${sourceDir}/cmake/x86_64-linux-gnu.cmake",
+            "cacheVariables"  : {"OS":"linux"}
+        }
+    ]
+}
+""")
+
+let cmake() =
+    CMakeLists
+    CMakePresets
+
+let dirs () = vscode doc src tmp giti rust cpp cmake
