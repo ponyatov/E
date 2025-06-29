d48d938c12843f6578ae214874d7b058f0ba0bc0
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-29 17:20:47 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/ai.mk
+++ mk/ai.mk
@@ -1,4 +1,4 @@
 .PHONY: ai
 ai: tmp/$(APP).ai.md
-tmp/$(APP).ai.md: README.md doc/$(APP).md $(F) $(MK)
-	cat README.md doc/$(APP).md $(F) > $@
+tmp/$(APP).ai.md: README.md doc/*.md lib/*.fs* $(MK)
+	cat README.md doc/*.md lib/*.fs* > $@
