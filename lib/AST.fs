0351bc69cdd31287b718477f6fe6b559184752bc
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-30 11:05:42 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/AST.fs
+++ lib/AST.fs
@@ -8,7 +8,19 @@ type Prim =
     | Int of int
     | Float of float
 
+/// operator
+type Op =
+    | Add of Expr * Expr
+    | Sub of Expr * Expr
+    | Mul of Expr * Expr
+    | Div of Expr * Expr
+
+/// expression
+and Expr =
+    | Prim of Prim
+    | Op of Op
+
 /// Abstract Syntax Tree
 type AST =
     | Comment of Comment
-    | Prim of Prim
+    | Expr of Expr
