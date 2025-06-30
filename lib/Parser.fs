edd4b33bdb5d192c869f62a82e0550813df648f5
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-30 11:45:17 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Parser.fs
+++ lib/Parser.fs
@@ -13,12 +13,13 @@ type Parser<'T> = Parser of (string -> PResult<'T * string>)
 /// helper function to run typed combinators
 let run (Parser p) (input: string) = p input
 
+/// parse single char
 let pchar (c: char) =
-    Parser(fun (s: string) ->
-        match s with
-        | "" -> Fail s
-        | s when s.[0] = c -> Succ(c, s.[1..])
-        | _ -> Fail s)
+    Parser(fun (input: string) ->
+        match input with
+        | "" -> Fail input
+        | input when input.[0] = c -> Succ(c, input.[1..])
+        | _ -> Fail input)
 
 let A = pchar 'A'
 
@@ -32,3 +33,17 @@ let B = pchar 'B'
 run B "" // Fail ""
 run B "A" // Fail "A"
 run B "BC" // Succ ('B', "C")
+
+/// parse string
+let pstr (s: string) =
+    Parser(fun (input: string) ->
+        match input with
+        | "" -> Fail input
+        | input when input.StartsWith(s) -> Succ(s, input.[s.Length ..])
+        | _ -> Fail input)
+
+let LineCommentPfx = pstr "//"
+
+run LineCommentPfx "" // Fail ""
+run LineCommentPfx "//" // Succ ("//", "")
+run LineCommentPfx "// A\n//" // Succ ("//", " A\n//")
