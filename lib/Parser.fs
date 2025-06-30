87f12237c30c68ff7a014a71a88d7fd6c389ef42
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-30 11:37:46 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Parser.fs
+++ lib/Parser.fs
@@ -8,7 +8,27 @@ type PResult<'T> =
     | Fail of string
 
 /// generic parser
-type Parser<'T> = Parser of (string -> 'T * string)
+type Parser<'T> = Parser of (string -> PResult<'T * string>)
 
 /// helper function to run typed combinators
-let run (Parser p) input = p input
+let run (Parser p) (input: string) = p input
+
+let pchar (c: char) =
+    Parser(fun (s: string) ->
+        match s with
+        | "" -> Fail s
+        | s when s.[0] = c -> Succ(c, s.[1..])
+        | _ -> Fail s)
+
+let A = pchar 'A'
+
+run A "" // Fail ""
+run A "A" // Succ ('A', "")
+run A "B" // Fail "B"
+run A "ABC" // Succ ('A', "BC")
+
+let B = pchar 'B'
+
+run B "" // Fail ""
+run B "A" // Fail "A"
+run B "BC" // Succ ('B', "C")
