8d9d3621ecceb4efdf0ea85768181bc093f6ef9f
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-30 11:13:58 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/AST.fs
+++ lib/AST.fs
@@ -13,9 +13,24 @@ let B = Int 456 // Int 456
 
 /// operator
 type Op =
+    /// prefix operators:
+    /// `+A`
+    | Plus of Expr
+    /// `-A`
+    | Neg of Expr
+    /// suffix operators:
+    /// `A++`
+    | Inc of Expr
+    /// `A--`
+    | Dec of Expr
+    /// infix operators:
+    /// `A+B`
     | Add of Expr * Expr
+    /// `A-B`
     | Sub of Expr * Expr
+    /// `A*B`
     | Mul of Expr * Expr
+    /// `A/B`
     | Div of Expr * Expr
 
 /// expression
