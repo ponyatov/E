d54ee7155e4f288b8f5883b75ce60269f9053194
Dmitry Ponyatov <dponyatov@gmail.com>
2025-06-30 11:52:17 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Parser.fs
+++ lib/Parser.fs
@@ -50,13 +50,12 @@ run LineCommentPfx "// A\n//" // Succ ("//", " A\n//")
 
 // https://fsharpforfunandprofit.com/posts/understanding-parser-combinators/#combining-two-parsers-in-sequence
 
-let andThen (p1:Parser) (p2:Parser) =
+let andThen p1 p2 =
     Parser(fun (input: string) ->
         match run p1 input with
         | Fail err -> Fail err // pass error
-        | Succ (val1,rest1) ->
+        | Succ(val1, rest1) ->
             match run p2 rest1 with
             | Fail err -> Fail err
-            | Succ (val2,rest2) ->
-                Succ((val1,val2),rest2)
-    )
+            | Succ(val2, rest2) -> //
+                Succ((val1, val2), rest2))
