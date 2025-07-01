9a8b9c2df843ff1cb8db9d2fa63004b31676258d
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-01 17:24:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Sestoft.fs
+++ lib/Sestoft.fs
@@ -16,3 +16,13 @@ let B = Int 456 // Int 456
 let ApB = BinOp(Add, A, B) // BinOp (Add, Int 123, Int 456)
 let AmB = BinOp(Mul, A, B) // BinOp (Mul, Int 123, Int 456)
 
+let rec eval (e: Expr) : int =
+    match e with
+    | Int n -> n
+    | BinOp(Add, e1, e2) -> eval e1 + eval e2
+    | _ -> failwith $"{e}"
+
+eval A // 123
+eval B // 456
+eval ApB // 579
+eval AmB // System.Exception: BinOp
