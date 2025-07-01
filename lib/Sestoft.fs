0f6567ab77bd9e30e77440d57da98251f845d7e5
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-01 17:22:06 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Sestoft.fs
+++ lib/Sestoft.fs
@@ -1,2 +1,18 @@
 // Programming Language Concepts for Software Developers
 // Peter Sestoft
+
+
+type Expr =
+    | Int of int
+    | BinOp of Op * Expr * Expr
+
+and Op =
+    | Add
+    | Mul
+
+let A = Int 123 // Int 123
+let B = Int 456 // Int 456
+
+let ApB = BinOp(Add, A, B) // BinOp (Add, Int 123, Int 456)
+let AmB = BinOp(Mul, A, B) // BinOp (Mul, Int 123, Int 456)
+
