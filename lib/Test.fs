0f5909772adf29d82c3917c36bff67749749a761
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-03 04:38:45 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Test.fs
+++ lib/Test.fs
@@ -2,14 +2,20 @@ module Test
 
 open Expecto
 
+open Sestoft
+
 [<Tests>]
 let tests =
-    testList
-        "core"
-        [ //
-          testCase "2+2"
-          <| fun _ -> //
-              Expect.equal (2 + 2) 4 "2+2 should be 4" ]
+    testList "core" [ //
+        testCase "2+2" <| fun _ -> //
+            Expect.equal (2 + 2) 4 "2+2 should be 4"
+        testCase "A" <| fun _ ->
+            let A = Int 123
+            Expect.equal A (Int 123)  "A"
+        testCase "B" <| fun _ ->
+            let B = Int 456
+            Expect.equal B (Int 456)  "B"
+    ]
 
 // [<EntryPoint>]
 // let main argv = runTestsWithCLIArgs [] argv tests
