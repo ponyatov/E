149aa3edff64193daf5b7a1b39a7b527b6e1b46b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-03 04:16:10 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Test.fs
+++ lib/Test.fs
@@ -2,10 +2,11 @@ module Test
 
 open Expecto
 
+[<Tests>]
 let tests =
     testList "core" [ //
         test "2+2" { //
             Expect.equal (2 + 2) 4 "2+2 should be 4" } ]
 
-[<EntryPoint>]
-let main argv = runTestsWithCLIArgs [] argv tests
+// [<EntryPoint>]
+// let main argv = runTestsWithCLIArgs [] argv tests
