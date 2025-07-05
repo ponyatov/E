eee33770d629fd3d633f20499ac65dc9feeadba1
Dmitry Ponyatov <dponyatov@gmail.com>
2025-07-05 11:43:00 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- lib/Sestoft.py
+++ lib/Sestoft.py
@@ -1,3 +1,9 @@
+## @file @brief pythonic OOP style code for book:
+## **Programming Language Concepts for Software Developers**
+## *Peter Sestoft*
+##
+## Object Graph-based homoiconic language (directed graph / tree)
+
 import os, sys, re
 
 ## test equality
@@ -30,17 +36,26 @@ class Object:
 
     def dump(self, depth=0, prefix=''):
         def tab(depth): return '\n' + '\t' * depth
-        ret = tab(depth) + self.head()
-        for i in self.nest: ret += i.dump(depth + 1)
+        ret = tab(depth) + self.head(depth, prefix)
+        for k in sorted(self.slot.keys()):
+            ret += self.slot[k].dump(depth + 1, f'{k} = ')
+        for i in self.nest:
+            ret += i.dump(depth + 1)
         return ret
 
     def __repr__(self): return self.dump()
 
-    def __floordiv__(self, that): self.nest.append(that); return self
+    def __floordiv__(self, that):
+        self.nest.append(that); return self
 
     def __getitem__(self, idx):
         if type(idx) == int: return self.nest[idx]
         if type(idx) == str: return self.slot[idx]
+        raise TypeError(type(idx), idx)
+
+    def __setitem__(self, idx, that):
+        if type(idx) == int: self.nest[idx] = that; return self
+        if type(idx) == str: self.slot[idx] = that; return self
         raise TypeError(type(idx), idx)
 
     def eval(self, env): return self
@@ -49,10 +64,10 @@ class Object:
 class Primitive(Object): pass
 
 ## abstract number
-class Num(Primitive): pass
+class Number(Primitive): pass
 
 ## integer numbers
-class Int(Num):
+class Int(Number):
     def __init__(self, N): super().__init__(int(N))
 
 A = Int(123)
@@ -62,8 +77,8 @@ test_eq(B, '\nint:456')
 # test_raise(Int("azaza")}'=='int: 123')
 
 ## floating point numbers
-class Float(Num):
-    def __init__(self, F): super().__init__(float(V))
+class Float(Number):
+    def __init__(self, F): super().__init__(float(F))
 
 class Container(Object): pass
 class Vector(Container): pass
@@ -84,12 +99,21 @@ test_eq(glob, '\nenv:glob')
 class Var(Primitive):
     def eval(self, env): return env[self.val()]
 
-Pi = Var('pi')
-test_eq(Pi, '\nvar:pi')
-test_eq(Pi.eval(glob), '\nfloat:3.1415')
+pi = Var('pi')
+test_eq(pi, '\nvar:pi')
+# test_raise(pi.eval(glob), KeyError)
+glob['pi'] = Float(3.1415)
+test_eq(glob, '\nenv:glob\n\tpi = float:3.1415')
+test_eq(pi.eval(glob), '\nfloat:3.1415')
 
 ## operator
 class Op(Active): pass
+
+## unary operator (prefix)
+class PfxOp(Op):
+    def __init__(self, V, A):
+        super().__init__(V); self // A
+
 ## binary operator
 class BinOp(Op):
     def __init__(self, V, A, B):
--- src/hello.e
+++ src/hello.e
@@ -1,3 +1,31 @@
-#!/usr/bin/env e
-// Script code follows...
-log 'Hello World!'
+#!/usr/bin/env Evento
+// ^^^^^^^^^^^^^^^^^^ optional shebang: first line only
+
+//! Sample Script code follows...
+/*! including language core tests */
+
+// module hello /* defined by file name & path */
+
+/// variables starts with lower letter (low_camel_case)
+const WHO = "World"
+
+/// multi-line strings can be wrapped in '' and ""
+/// - $"" $'' raw strings, no escapes
+/// - @"" @'' format & string interpolation
+
+let hello = $'Hello {who}!'
+
+/// internal logger in code language, traces program execution
+/// default logger prints to stderr
+/// can be reassigned or duplicated to other streams
+/// stdout/stderr streams can be used directly (OS level output)
+log hello ; stderr hello
+
+/// check last log lines
+test_log "Hello, World!"
+
+let loggers = [log;stderr]
+/// - apply functions using container iteration
+for f in loggers: f hello
+/// - using map, lambda function and pipe operator
+loggers.map |f| -> hello |> f
